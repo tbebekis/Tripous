ab07d7984d1df06b4c405415fb3bc7207ba790cd
Theo Bebekis <teo.bebekis@gmail.com>
2021-01-05 02:12:25 +0200

    .

--- Tripous.Web/Session.cs
+++ Tripous.Web/Session.cs
@@ -52,6 +52,7 @@ namespace Tripous.Web
         /* public */
         /// <summary>
         /// Returns a value stored in session, found under a specified key or a default value if not found.
+        /// <para>NOTE: Key is NOT case sensitive.</para>
         /// </summary>
         static public T Get<T>(string Key)
         {
@@ -59,6 +60,7 @@ namespace Tripous.Web
         }
         /// <summary>
         /// Returns a value stored in session, found under a specified key or a default value if not found.
+        /// <para>NOTE: Key is NOT case sensitive.</para>
         /// </summary>
         static public T Get<T>(string Key, T Default)
         {
@@ -66,6 +68,10 @@ namespace Tripous.Web
         }
         /// <summary>
         /// Stores a value in session under a specified key.
+        /// <para>NOTE: Key is NOT case sensitive.</para>
+        /// <para>WARNING: Whenever an object is added by calling Set(), the object is serialized.
+        /// So adding the object first and then altering the object will NOT work.
+        /// The object should be added after any alteration to it is done.</para>
         /// </summary>
         static public void Set<T>(string Key, T Value)
         {
@@ -73,7 +79,20 @@ namespace Tripous.Web
         }
 
         /// <summary>
+        /// Removes and returns a value found under a specified key, if any, else returns the default value of the specified type argument.
+        /// <para>NOTE: Key is NOT case sensitive.</para>
+        /// </summary>
+        static public T Pop<T>(string Key)
+        {
+            T Result = Get<T>(Key);
+            Remove(Key);
+            return Result;
+        }
+
+
+        /// <summary>
         /// Returns a string stored in session, found under a specified key or null if not found.
+        /// <para>NOTE: Key is NOT case sensitive.</para>
         /// </summary>
         static public string GetString(string Key)
         {
@@ -81,6 +100,7 @@ namespace Tripous.Web
         }
         /// <summary>
         /// Stores a string value in session under a specified key.
+        /// <para>NOTE: Key is NOT case sensitive.</para>
         /// </summary>
         static public void SetString(string Key, string Value)
         {
@@ -96,6 +116,7 @@ namespace Tripous.Web
         }
         /// <summary>
         /// Removes a session variable under a specified key.
+        /// <para>NOTE: Key is NOT case sensitive.</para>
         /// </summary>
         static public void Remove(string Key)
         {
@@ -104,6 +125,7 @@ namespace Tripous.Web
         }
         /// <summary>
         /// Returns true if a variable exists in session under a specified key.
+        /// <para>NOTE: Key is NOT case sensitive.</para>
         /// </summary>
         static public bool ContainsKey(string Key)
         {
